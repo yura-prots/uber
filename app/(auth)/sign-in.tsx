e9396170dcd4bc5198ff0460b5c493ea589b3a2d
yura-prots <yuraprots@gmail.com>
2024-08-25 14:10:46 +0300

    created directories and files

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign In</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  );
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
