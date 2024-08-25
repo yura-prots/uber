544c1834938e313a07ce672217dcb407edb89f6e
yura-prots <yuraprots@gmail.com>
2024-08-25 20:11:11 +0300

    add images, titles, descriptions

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -3,7 +3,7 @@
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
@@ -32,9 +32,23 @@ const Onboarding = () => {
         onIndexChanged={(index) => setActiveIndex(index)}
       >
         {onboarding.map((item) => (
-          // <View>
-          <Text>{item.title}</Text>
-          // </View>
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
         ))}
       </Swiper>
     </SafeAreaView>
